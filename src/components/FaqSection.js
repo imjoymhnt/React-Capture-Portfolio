e5e121cd0e659968389c04e75f30f63aedca1c6c
Mritunjoy <mritunjoy.mahanta001@gmail.com>
2020-12-12 01:41:55 +0530

    Added toggle feature in the faq section and also created a separate reusable component for the toggle

--- src/components/FaqSection.js
+++ src/components/FaqSection.js
@@ -1,43 +1,46 @@
 import React from 'react';
 import styled from 'styled-components';
 import { About } from '../styles';
+import Toggle from './Toggle';
+import { AnimateSharedLayout } from 'framer-motion';
 
 const FaqSection = () => {
     return (
         <Faq>
             <h2>Any Questions <span>FAQ</span></h2>
-            <div className="question">
-                <h4>How do I start?</h4>
+            <AnimateSharedLayout>
+            <Toggle title='How do I start?'>
+            
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, laudantium.</p>
+                
+                </div>
+            </Toggle>
+            <Toggle title='Daily Schedule?'>
+            
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, laudantium.</p>
+                
+                </div>
+            </Toggle>
+            <Toggle title='Different Payment methos we accept?'>
+            
+                <div className="answer">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, laudantium.</p>
+                
+                </div>
+                </Toggle>
+            <Toggle title='What products do we offer?'>
+            
                 <div className="answer">
                     <p>Lorem ipsum dolor sit amet.</p>
                     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, laudantium.</p>
                 </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Daily Schedule?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, laudantium.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>Different Payment methos we accept?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, laudantium.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
-            <div className="question">
-                <h4>What products do we offer?</h4>
-                <div className="answer">
-                    <p>Lorem ipsum dolor sit amet.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut, laudantium.</p>
-                </div>
-                <div className="faq-line"></div>
-            </div>
+                </Toggle>
+                </AnimateSharedLayout>
         </Faq>
     )
 }
